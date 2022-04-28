b86c679fb684010379c375971bdd3b0a1d03c8f1
s205347 <s205347@student.dtu.dk>
2022-04-28 12:45:17 +0200

    Should add ViewModel and Detail folders

---
+++ Recipe World/Views/Detail/tes123.swift
@@ -0,0 +1,20 @@
+//
+//  tes123.swift
+//  Recipe World
+//
+//  Created by Nicklas Olabi on 28/04/2022.
+//
+
+import SwiftUI
+
+struct tes123: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct tes123_Previews: PreviewProvider {
+    static var previews: some View {
+        tes123()
+    }
+}
